51ea54a9c2764a08d05925745d8539af57259af9
Laurent Peuch <cortex@worlddomination.be>
2015-12-09 08:18:42 +0100

    [enh] add a setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding:Utf-8 -*-
+
+from setuptools import setup
+
+setup(name='prosopopee',
+      version='0.1',
+      description='excposure.co clone in a static web generating tool',
+      author='Laurent Peuch',
+      #long_description='',
+      author_email='cortex@worlddomination.be',
+      url='https://github.com/Psycojoker/prosopopee',
+      install_requires=open("./requirements.txt", "r").split(),
+      packages=[],
+      py_modules=[],
+      license= 'GPLv3+',
+      scripts=['prosopopee'],
+      keywords='',
+     )
