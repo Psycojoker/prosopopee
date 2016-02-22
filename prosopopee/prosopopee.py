8c7dbc5a4a6f843ce62d9798e0e2e63bf230c2b7
Adrien Beudin <beudbeud@beudibox.fr>
2016-02-22 14:11:36 +0100

    add support theme

--- prosopopee/prosopopee.py
+++ prosopopee/prosopopee.py
@@ -118,7 +118,16 @@ def main():
     # XXX recursively merge directories
     if os.path.exists(os.path.join(os.getcwd(), "build", "static")):
         shutil.rmtree(os.path.join(os.getcwd(), "build", "static"))
-    if os.path.exists(os.path.join(os.getcwd(), "static")):
+    if settings.get("theme"):
+        if os.path.exists(os.path.join(os.getcwd(), "templates")):
+            shutil.rmtree(os.path.join(os.getcwd(), "templates"))
+        shutil.copytree(os.path.join(os.path.split(os.path.realpath(__file__))[0], "themes", settings.get("theme"), "static"), os.path.join(os.getcwd(), "build", "static"))
+        shutil.copytree(os.path.join(os.path.split(os.path.realpath(__file__))[0], "themes", settings.get("theme"), "templates"), os.path.join(os.getcwd(), "templates"))
+        templates = Environment(loader=FileSystemLoader([os.path.realpath(os.path.join(os.getcwd(), "templates")), os.path.join(os.path.split(os.path.realpath(__file__))[0], "templates")]))
+        index_template = templates.get_template("index.html")
+        gallery_index_template = templates.get_template("gallery-index.html")
+        page_template = templates.get_template("page.html")
+    elif os.path.exists(os.path.join(os.getcwd(), "static")):
         shutil.copytree(os.path.join(os.getcwd(), "static"), os.path.join(os.getcwd(), "build", "static"))
     else:
         shutil.copytree(os.path.join(os.path.split(os.path.realpath(__file__))[0], "static"), os.path.join(os.getcwd(), "build", "static"))
