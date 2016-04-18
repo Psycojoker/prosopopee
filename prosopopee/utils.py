c372d899befb152f1452cc3762e2f0bc85346ae6
Laurent Peuch <cortex@worlddomination.be>
2016-04-18 20:00:50 +0200

    [mod] no space before : in english

--- prosopopee/utils.py
+++ prosopopee/utils.py
@@ -12,16 +12,16 @@ def error(test, error_message):
     if test:
         return
 
-    sys.stderr.write(bcolors.FAIL + "Abort : " + bcolors.ENDC + error_message)
+    sys.stderr.write(bcolors.FAIL + "Abort: " + bcolors.ENDC + error_message)
     sys.stderr.write("\n")
     sys.exit(1)
 
 
 def warning(logging, warning_message):
-    sys.stderr.write("%s%s : %s%s" % (bcolors.WARNING, logging, bcolors.ENDC, warning_message))
+    sys.stderr.write("%s%s: %s%s" % (bcolors.WARNING, logging, bcolors.ENDC, warning_message))
     sys.stderr.write("\n")
 
 
 def okgreen(logging, ok_message):
-    sys.stderr.write("%s%s : %s%s" % (bcolors.OKGREEN, logging, bcolors.ENDC, ok_message))
+    sys.stderr.write("%s%s: %s%s" % (bcolors.OKGREEN, logging, bcolors.ENDC, ok_message))
     sys.stderr.write("\n")
