2eb4f8a15a415150093c976b4ff2389d21695123
Thomas Nonglaton <titoko@titoko.fr>
2017-06-03 13:23:56 +0200

    Allow reverse & normal order in index

--- prosopopee/prosopopee.py
+++ prosopopee/prosopopee.py
@@ -391,7 +391,7 @@ def process_directory(gallery_name, settings, parent_templates, parent_gallery_p
                     process_directory(subgallery.name, settings, subgallery_templates, gallery_path)
                 )
 
-            build_index(settings, sub_page_galleries_cover, subgallery_templates, gallery_path, sub_index=True)
+            build_index(settings, sub_page_galleries_cover, subgallery_templates, gallery_path, sub_index=True, gallery_settings=gallery_settings)
             gallery_cover['sub_gallery'] = sub_page_galleries_cover
 
     return gallery_cover
@@ -496,10 +496,13 @@ def build_gallery(settings, gallery_settings, gallery_path, template):
         open(Path("build").joinpath(gallery_light_path, "index.html"), "w").write(html)
 
 
-def build_index(settings, galleries_cover, templates, gallery_path='', sub_index=False):
+def build_index(settings, galleries_cover, templates, gallery_path='', sub_index=False, gallery_settings={}):
     index_template = templates.get_template("index.html")
-
-    galleries_cover = reversed(sorted(filter(lambda x: x != {}, galleries_cover), key=lambda x: x["date"]))
+    reverse = gallery_settings.get('reverse', settings.get('reverse', True))
+    if reverse:
+        galleries_cover = reversed(sorted(filter(lambda x: x != {}, galleries_cover), key=lambda x: x["date"]))
+    else:
+        galleries_cover = sorted(filter(lambda x: x != {}, galleries_cover), key=lambda x: x["date"])
 
     # this should probably be a factory
     Image.base_dir = Path(".").joinpath(gallery_path)
